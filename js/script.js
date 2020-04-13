66722aff5760f0a580c3fb5eff94b34357f793d9
Pranav Ashok <pranavashok@gmail.com>
2020-04-13 23:30:47 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -458,7 +458,8 @@ function lookup(inputString) {
 				tmp = tmp.replace(' ', '_');
 
 				$('#painting-events').hide();
-				$('#painting-events').css('background', $('div#'+ tmp).css('background'));
+				$('#painting-events').css('background-image', $('div#'+ tmp).css('background-image'));
+				$('#painting-events').css('background-position', $('div#'+ tmp).css('background-position'));
 				$('#painting-events').css('background-repeat', 'no-repeat repeat');
 				if ($('#inner-pane-events').attr('class') != 'moveright')
 					$('#painting-events').stop(true, true).fadeIn(250);
