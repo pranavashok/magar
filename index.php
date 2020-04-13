715355afebcf1225a0fac79111eeda98b19debe4
Pranav Ashok <pranavashok@gmail.com>
2020-04-13 23:40:33 +0200

    reachus placement

--- index.php
+++ index.php
@@ -532,7 +532,8 @@ require_once('root.php'); ?>
 				<form id="form" name="form" method="post" action="javascript:feedback()">
 				</form>
 				
-				<p></p><br /><br/><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.179538060995!2d75.93144721555139!3d11.32158435198812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba642fd50000001%3A0xbe8a77db953bda6c!2sNIT%20Calicut!5e0!3m2!1sen!2sde!4v1586790897700!5m2!1sen!2sde" width="400" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe><br>
+				<p></p><br /><br/>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.179538060995!2d75.93144721555139!3d11.32158435198812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba642fd50000001%3A0xbe8a77db953bda6c!2sNIT%20Calicut!5e0!3m2!1sen!2sde!4v1586790897700!5m2!1sen!2sde" width="400" height="350" frameborder="0" style="border:0;float:left;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe><br>
 			
 				<div style="padding:0px 0px 0px 430px; align:right; font-size:10pt; ">
 
